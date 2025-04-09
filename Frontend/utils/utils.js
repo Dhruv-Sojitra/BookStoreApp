c50c764684c116d95115fce2bd8c46780ab2cba5
Dhruv Sojitra <dhruvg2336@gmai.com>
2025-04-09 09:27:14 +0530

    file update

--- Frontend/utils/utils.js
+++ Frontend/utils/utils.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "https://bookstoreapp-uzvo.onrender.com/";
-export const BACKEND_URL = "http://localhost:4001";
+export const BACKEND_URL = "https://book-store-app-backend1.vercel.app";
