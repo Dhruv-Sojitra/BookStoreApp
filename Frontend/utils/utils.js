eb02ad11b80b320279d15eb02b7af8782eef65d2
Dhruv Sojitra <dhruvg2336@gmai.com>
2025-04-09 09:21:04 +0530

    file update

--- Frontend/utils/utils.js
+++ Frontend/utils/utils.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "https://bookstoreapp-uzvo.onrender.com/";
+// export const BACKEND_URL = "https://bookstoreapp-uzvo.onrender.com/";
+export const BACKEND_URL = "http://localhost:4001";
